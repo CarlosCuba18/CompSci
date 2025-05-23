aa05fb0d44b73f07a618a409d689c222f10f8278
Carlos Cuba <carlos.cuba.412@my.csun.edu>
2025-05-22 21:05:22 -0700

    beginnint to create a gitignore

--- personal/gui_practice/MyFrame.java
+++ personal/gui_practice/MyFrame.java
@@ -3,17 +3,13 @@ import java.awt.Color; // color import
 import javax.swing.ImageIcon; //image icon import
 
 public class MyFrame extends JFrame{ //makes MyFrame a child of JFrame, so it can call methods from JFrame
-
 	MyFrame(){
 		this.setTitle("JFrame Title goes here"); //sets title of frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // makes X close the entire program (default is HIDE_ON_CLOSE)
-		this.setResizable(false); // makes it not possible to change size
+		this.setResizable(false); // makes it (not)possible to change size
 		this.setSize(420,420); //sets x and y dimentions
 		this.setVisible(true); //makes it see-able
-		this.getContentPane().setBackground(Color.green); //sets color of background
-		//frame.getContentPane().setBackground(new Color(a)); where a is a RGB value OR hex color value
-		// new Color(0,0,0) makes black (all ranges of 0-255) in order of red,green,blue
-		// new Color(0xFFFFFF) makes white	
-
+		this.getContentPane().setBackground(Color.blue); //sets color of background
+		//frame.getContentPane().setBackground(new Color(a)); where a is a RGB value OR hex color value	
 	}//end of constructor
 }// end of class
