af5ac7087e83332459b04230c74b3a5a82020fce
Carlos Cuba <carlos.cuba.412@my.csun.edu>
2025-05-28 14:38:32 -0700

    adding picture to frame

--- personal/gui_practice/MyFrame.java
+++ personal/gui_practice/MyFrame.java
@@ -4,14 +4,20 @@ import javax.swing.ImageIcon; //image icon import
 
 public class MyFrame extends JFrame{ //makes MyFrame a child of JFrame, so it can call methods from JFrame
 	MyFrame(){
+		//JFrame frame = new JFrame(); //creates new frame; in this case MyFrame extends Jframe so no object needs to be created
 		this.setTitle("JFrame Title goes here"); //sets title of frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // makes X close the entire program (default is HIDE_ON_CLOSE)
 		this.setResizable(false); // makes it (not)possible to change size
 		this.setSize(420,420); //sets x and y dimentions
 		this.setVisible(true); //makes it see-able
 		this.getContentPane().setBackground(Color.blue); //sets color of background
+		//all to change image in corner of page
+		ImageIcon image = new ImageIcon("image.png"); //put path of image or name if in same file into argument
+		this.setIconImage(image.getImage()); //changes image in corner
 		//frame.getContentPane().setBackground(new Color(a)); where a is a RGB value OR hex color value	
-	}//end of constructor
+			// new Color(0,0,0) makes black (all ranges of 0-255) in order of red,green,blue
+			// new Color(0xFFFFFF) makes white
+	} //end of constructor
 
-	
+
 }// end of class
