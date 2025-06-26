337394606f47b8154fdfa9e20349286ad32d27f5
Carlos Cuba <carlos.cuba.412@my.csun.edu>
2025-06-26 11:10:22 -0700

    working with panels

--- personal/gui_practice/GUIPractice.java
+++ personal/gui_practice/GUIPractice.java
@@ -5,50 +5,41 @@ import javax.swing.JLabel;
 import javax.swing.border.Border;//border import
 import javax.swing.BorderFactory; //import border factory
 import javax.swing.border.LineBorder;
+import javax.swing.JPanel;
 
 public class GUIPractice{
 	public static void main(String[] args){
 		//MyFrame myFrame = new MyFrame();
+		//myFrame.makeLabel();
 
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null); //gonna learn about layout managers later
 		frame.setVisible(true);
-		frame.setSize(1000,1000);
+		frame.setSize(750,750);
 
-		//making label; label needs frame^
-		JLabel label = new JLabel(); //creates label; can pass in text instead of setText
-		label.setText("Testing Label"); //sets text
-		frame.add(label); // adds label to frame
+		JPanel redPanel = new JPanel();
+		redPanel.setBackground(Color.red);
+		redPanel.setBounds(0,0,250,250); //(x,y,width,height)with no layout manager, need to set bounds and locations manually
+		frame.add(redPanel);
 
-		//adding image to label
-		ImageIcon image = new ImageIcon("picture.png"); // works for Gif, jpeg, png, and xbm
-		label.setIcon(image); //puts image inside frame
+		JPanel bluePanel = new JPanel();
+		bluePanel.setBackground(Color.blue);
+		bluePanel.setBounds(250,0,250,250);
+		frame.add(bluePanel);
 
-		//sets location of texts
-		label.setHorizontalTextPosition(JLabel.CENTER); //Jlabel.* with * being LEFT, CENTER, RIGHT of image icon
-		label.setVerticalTextPosition(JLabel.TOP); // TOP, CENTER, or BOTTOM of icon
+		JPanel greenPanel = new JPanel();
+		greenPanel.setBackground(Color.green);
+		greenPanel.setBounds(0,250,500,250);
+		frame.add(greenPanel);
 
-		//sets text's color and font(fix font)
-		label.setForeground(Color.green); //pass in color with set constants, hex value, or RGB
-		//label.setFont(new Font("MV Boli",Font.PLAIN,20)); //set font
-		label.setIconTextGap(-25); //set gap of text to image
+		JLabel label = new JLabel();
+		label.setText("Hello");
+		ImageIcon icon = new ImageIcon("picture.png");
+		label.setIcon(icon);
+		redPanel.add(label);
 
-		//set background color
-		label.setBackground(Color.black); //set background color
-		label.setOpaque(true); //display background color
 
-		//making border(fix later)
-		//Border border = new BorderFactory.createLineBorder(Color.blue, 3); //makes border color, passes in color and width of border
-		//label.setBorder(border);
-
-		//moving image
-		label.setVerticalAlignment(JLabel.CENTER);
-		label.setHorizontalAlignment(JLabel.CENTER);
-
-		//make label a fixed size (works but using pack method)
-		//frame.setLayout(null);
-		//label.setBounds(0,0,250,250); //(x,y,dimention,dimention); x,y is starting pt, dimentions are how big it is
-		frame.pack();//automatically adjust frame to things inside label; use this method last after making method
 
 
 
--- personal/gui_practice/MyFrame.java
+++ personal/gui_practice/MyFrame.java
@@ -1,9 +1,16 @@
 import javax.swing.JFrame; //import for frame
 import java.awt.Color; // color import
 import javax.swing.ImageIcon; //image icon import
+import javax.swing.JLabel;
+import javax.swing.border.Border;//border import
+import javax.swing.BorderFactory; //import border factory
+import javax.swing.border.LineBorder;
 
 public class MyFrame extends JFrame{ //makes MyFrame a child of JFrame, so it can call methods from JFrame
 	MyFrame(){
+	} //end of constructor
+
+	public void makeFrame(){
 		//JFrame frame = new JFrame(); //creates new frame; in this case MyFrame extends Jframe so no object needs to be created
 		this.setTitle("JFrame Title goes here"); //sets title of frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // makes X close the entire program (default is HIDE_ON_CLOSE)
@@ -17,7 +24,50 @@ public class MyFrame extends JFrame{ //makes MyFrame a child of JFrame, so it ca
 		//frame.getContentPane().setBackground(new Color(a)); where a is a RGB value OR hex color value	
 			// new Color(0,0,0) makes black (all ranges of 0-255) in order of red,green,blue
 			// new Color(0xFFFFFF) makes white
-	} //end of constructor
+	}
+	public void makeLabel(){
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(1000,1000);
+
+		//making label; label needs frame^
+		JLabel label = new JLabel(); //creates label; can pass in text instead of setText
+		label.setText("Testing Label"); //sets text
+		frame.add(label); // adds label to frame
+
+		//adding image to label
+		ImageIcon image = new ImageIcon("picture.png"); // works for Gif, jpeg, png, and xbm
+		label.setIcon(image); //puts image inside frame
+
+		//sets location of texts
+		label.setHorizontalTextPosition(JLabel.CENTER); //Jlabel.* with * being LEFT, CENTER, RIGHT of image icon
+		label.setVerticalTextPosition(JLabel.TOP); // TOP, CENTER, or BOTTOM of icon
+
+		//sets text's color and font(fix font)
+		label.setForeground(Color.green); //pass in color with set constants, hex value, or RGB
+		//label.setFont(new Font("MV Boli",Font.PLAIN,20)); //set font
+		label.setIconTextGap(-25); //set gap of text to image
+
+		//set background color
+		label.setBackground(Color.black); //set background color
+		label.setOpaque(true); //display background color
+
+		//making border(fix later)
+		//Border border = new BorderFactory.createLineBorder(Color.blue, 3); //makes border color, passes in color and width of border
+		//label.setBorder(border);
+
+		//moving image
+		label.setVerticalAlignment(JLabel.CENTER);
+		label.setHorizontalAlignment(JLabel.CENTER);
+
+		//make label a fixed size (works but using pack method)
+		//frame.setLayout(null);
+		//label.setBounds(0,0,250,250); //(x,y,dimention,dimention); x,y is starting pt, dimentions are how big it is
+		frame.pack();//automatically adjust frame to things inside label; use this method last after adding everything else (reason why its small lol)
+	}
+
+
 
 
 }// end of class
